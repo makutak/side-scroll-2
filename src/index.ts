ae4c8c2847c579c7bbc899adf352ca51d299c750
makutak <takkouno@gmail.com>
2020-07-02 22:49:59 +0900

    円を描画できる

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-alert('hello');
+const canvas: HTMLCanvasElement = document.getElementById('app') as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.arc(10, 10, 10, 0, (Math.PI * 2));
+ctx.fillStyle = ('#0095DD');
+ctx.fill();
+ctx.closePath();
