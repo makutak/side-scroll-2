959263028ba88342091a766586b350ea437fbd29
makutak <takkouno@gmail.com>
2020-07-10 02:29:46 +0900

    削除

--- src/Player.ts
+++ src/Player.ts
@@ -24,27 +24,11 @@ export class Player {
 
   draw(): void {
     //console.log('player draw; ');
-    // this.ctx.beginPath();
-    // //this.ctx.arc(this.positionX, this.positionY, 0, Math.PI * 2, false);
-    // this.ctx.arc(
-    //   this.positionX,
-    //   this.positionY,
-    //   this.ballRadius,
-    //   0,
-    //   Math.PI * 2,
-    //   false
-    // );
-    // this.ctx.fillStyle = '#0095DD';
-    // this.ctx.fill();
-    // this.ctx.closePath();
     //this.ctx.save();
     //this.ctx.transform(-1, 0, 0, 1, 0, 0)
     //console.log(this.positionY)
-    this.ctx.drawImage(this.img, this.offsetX, 0, this.offsetX, this.height, this.positionX, this.positionY, this.height, this.height,);
+    this.ctx.drawImage(this.img, this.offsetX, 0, this.offsetX, this.height, this.positionX, this.positionY, this.height, this.height);
     //this.ctx.drawImage(this.img, 0, 0, 32, 32, -0 - 32, 416, 32, 32);
     //this.ctx.restore();
-    //console.log(this.img);
-    //this.ctx.restore()
-    //console.log(this.tex)
   }
 }
