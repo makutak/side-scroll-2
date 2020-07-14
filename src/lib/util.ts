72ef1abaedbc73d19b170cdfa5c19297119652ab
makutak <takkouno@gmail.com>
2020-07-14 22:37:06 +0900

    Added helper function.

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,21 @@
-export function staticImplements<T>() {
+export const staticImplements = <T>() => {
   return (constructor: T) => { }
+};
+
+interface Attr {
+  class: string;
 }
+
+export const elt = (name: string, attrs: Attr[], ...children: Node[]): HTMLElement => {
+  let dom = document.createElement(name);
+
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attr[attr]);
+  }
+
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+
+  return dom;
+};
