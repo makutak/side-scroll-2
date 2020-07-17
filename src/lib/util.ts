4ad172a18d1b2bf8606f213a1d1c6312b41eb3a9
makutak <takkouno@gmail.com>
2020-07-18 05:30:49 +0900

    Formatted

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -40,7 +40,7 @@ export const drawGrid = (level: Level) => {
 
 export const drawActors = (actors: Actor[]) => {
   return elt('div', {}, ...actors.map((actor: Actor) => {
-    let rect = elt('div', {class: `actor ${actor.type}`});
+    let rect = elt('div', { class: `actor ${actor.type}` });
     rect.style.width = `${actor.size.x * scale}px`;
     rect.style.height = `${actor.size.y * scale}px`;
     rect.style.left = `${actor.pos.x * scale}px`;
