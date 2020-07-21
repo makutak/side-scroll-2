a560149a2ecd460b093cf1ac39e47e3ef59b60a5
makutak <takkouno@gmail.com>
2020-07-22 03:35:51 +0900

    Added function

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,25 @@ export class Level {
       })
     })
   }
+
+  touches(pos: Vec, size: Vec, type: string): boolean {
+    const xStart = Math.floor(pos.x);
+    const xEnd = Math.floor(pos.x + size.x);
+    const yStart = Math.floor(pos.y);
+    const yEnd = Math.floor(pos.y + size.y);
+
+    for (let y = yStart; y < yEnd; y++) {
+      for (let x = xStart; x < xEnd; x++) {
+        let isOutside =
+          x < 0 || x >= this.width ||
+          y < 0 || y >= this.height;
+
+        let here = isOutside ? 'wall' : this.rows[y][x];
+        if (here === type) return true;
+      }
+      return false;
+    }
+  }
 }
 
 enum Status {
