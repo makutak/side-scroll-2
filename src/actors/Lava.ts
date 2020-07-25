df352299834476115b9b24c60ac010d60ebee54c
makutak <takkouno@gmail.com>
2020-07-25 16:11:03 +0900

    Implemented Lava update method.

--- src/actors/Lava.ts
+++ src/actors/Lava.ts
@@ -20,7 +20,15 @@ export class Lava extends Actor implements GameObject {
   get type(): string { return ActorType.LAVA; }
 
   update(time: number, state: State) {
-    return this;
+    let newPos = this.pos.plus(this.speed.times(time));
+
+    if (!state.level.touches(newPos, this.size, 'wall')) {
+      return new Lava(newPos, this.speed, this.reset);
+    } else if (this.reset) {
+      return new Lava(this.reset, this.speed, this.reset);
+    } else {
+      return new Lava(this.pos, this.speed.times(-1));
+    }
   }
 
   collide(state: State): State {
